04c6b08966a20839c4e51509027db8e5b6b1c233
msmaitran <itsmaisn@gmail.com>
2019-10-17 14:24:49 -0700

    Add dependencies.

--- M04/GuidedProject/Conductor/app/src/main/java/com/lambdaschool/conductor/controllers/BaseController.kt
+++ M04/GuidedProject/Conductor/app/src/main/java/com/lambdaschool/conductor/controllers/BaseController.kt
@@ -1,9 +1,6 @@
 package com.lambdaschool.conductor.controllers
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
 
 abstract class BaseController: Controller {
